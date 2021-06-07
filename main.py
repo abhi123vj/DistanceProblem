749db99fa4bbd99d492313f7b8e1dd32e090bb82
4bh16am <abhiram123vj@gmail.com>
2021-06-07 08:51:00 +0530

    added cmnts

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 import json
 import math
 
+#reading Data from file
 f = open('Json/data.json',)
 data = json.load(f)
-print("hi")
+#initi var
 count = 0
 store_list = []
 newls = []
+#looping through Json file
 for item in data:
     store_details = {"latlng":None, "name":None,"population":None}
     store_details['latlng'] = item['latlng']
     store_details['name'] = item['name']
     store_details['population'] = item['population']
+    #checks for populationcondition
     if item['population'] >= 847000 and count <20:
         count=count +1
         dif=847000-item['population'] 
         print(count,item['name'],item['population'],dif,item['latlng'])
         newls.append(store_details['latlng'])
+        #saves latitude country and populaton to store_details
     store_list.append(store_details)
-#print(store_list)
-print(newls)
-print(newls[0][0])
 
+#defines  function to find distace between 2 coordinate
 def dis(i,j):
     lat1,lon1=newls[i]
     lat2,lon2=newls[j]
@@ -34,7 +36,7 @@ def dis(i,j):
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
     d = radius * c
     return d
-print("strt")
+#finding all the sum of distance
 s=0.00
 for i in range(0,20):
     for j in range(i,20):
@@ -42,10 +44,5 @@ for i in range(0,20):
             d= round(dis(i,j),2)
             s=s+round(d,2)
             
-print(round(s,2))
+print(round(s,2)) #printing results
 f.close()
-
-#1619949.7200000002
-#1619950.4000000001
-#1619949.8039999995
-#1619949.8109
